573bb825330d71421a6c73381f99a7c0613d2012
Diego Valencia <91valenciadiego@gmail.com>
2026-05-24 15:59:20 -0500

    Creating Log

---
+++ PlainFiles/Core/LogWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core;
+public class LogWriter : IDisposable
+{
+
+    private readonly StreamWriter _writer;
+
+    public bool AutoFlush { get; }
+
+    public LogWriter(string path)
+    {
+        _writer = new StreamWriter(path, append: true);
+        {
+            AutoFlush = true;
+        };  
+    }
+
+    public void WriteLog(string level, string message)
+    {
+        var timeStamp = DateTime.Now.ToString("s");
+        var logEntry = $"[{timeStamp}] [{level}] {message}";
+        _writer.WriteLine(logEntry);
+    }
+    public void Dispose()
+    {
+        _writer.Dispose();
+    }
+}
--- PlainFiles/UI/Program.cs
+++ PlainFiles/UI/Program.cs
@@ -2,74 +2,101 @@
 using Core;
 
 var texFile = new SimpleTextFile("D:\\Documentos\\ITM\\Cuarto Semestre\\ESTRUCTURA DE DATOS Y LABORATORIO\\Unidad 4\\tmp\\data.txt");
-var lines = texFile.ReadLines();
-var list = lines.ToList();
-var option = string.Empty;
-
-do
+using var logger = new LogWriter("D:\\Documentos\\ITM\\Cuarto Semestre\\ESTRUCTURA DE DATOS Y LABORATORIO\\Unidad 4\\tmp\\log.txt");
+try
 {
-  option = Menu();
-  switch (option)
+    logger.WriteLog("INFO", "Application started.");
+
+    var lines = texFile.ReadLines();
+    var list = lines.ToList();
+    var option = string.Empty;
+
+    do
     {
-        case "1":
-            Console.WriteLine("Shoe lines:");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            break;
-
-        case "2":
-            Console.Write("Enter a new line: ");
-            var newLine = Console.ReadLine();
-            list.Add(newLine!);
-            break;
-
-        case "3":
-            texFile.WriteAllLines(list.ToArray());
-            Console.WriteLine("Changes saved.");
-            break;
-
-        case "4":
-            Console.Write("Enter the line to remove: ");
-            var lineToRemove = Console.ReadLine();
-            if (list.Remove(lineToRemove!))
-            {
-                Console.WriteLine("Line removed.");
-            }
-            else
-            {
-                Console.WriteLine("Line not found.");
-            }
-            break;
-
-        case "5":
-            Console.Write("Enter the word to remove:");
-            var wordToRemove = Console.ReadLine();
-            for (int i = list.Count - 1; i >= 0; i--)
-            {
-                if (list[i].Contains(wordToRemove!))
+        option = Menu();
+        switch (option)
+        {
+            case "1":
+                Console.WriteLine("Shoe lines:");
+                foreach (var item in list)
                 {
-                    list.RemoveAt(i);
+                    Console.WriteLine(item);
                 }
-            }
-            break;
+                logger.WriteLog("INFO", "Displayed lines to the user.");
+                break;
 
-        case "6":
-            list.Sort();
-            Console.WriteLine("Lines sorted.");
-            break;
-        case "0":
-            Console.WriteLine("Exiting...");
-            break;
+            case "2":
+                Console.Write("Enter a new line: ");
+                var newLine = Console.ReadLine();
+                list.Add(newLine!);
+                logger.WriteLog("INFO", $"Added a new line to the list. {newLine}");
+                break;
 
-        default:
-            Console.WriteLine("Invalid option. Please try again.");
-            break;
-    }
-} while (option != "0");
-texFile.WriteAllLines(list.ToArray());
-Console.WriteLine("Changes saved.");
+            case "3":
+                texFile.WriteAllLines(list.ToArray());
+                Console.WriteLine("Changes saved.");
+                logger.WriteLog("INFO", "Saved changes to the file.");
+                break;
+
+            case "4":
+                Console.Write("Enter the line to remove: ");
+                var lineToRemove = Console.ReadLine();
+                if (list.Remove(lineToRemove!))
+                {
+                    Console.WriteLine("Line removed.");
+                    logger.WriteLog("INFO", $"Removed a line from the list. {lineToRemove}");
+                }
+                else
+                {
+                    Console.WriteLine("Line not found.");
+                    logger.WriteLog("WARNING", $"Attempted to remove a line that was not found. {lineToRemove}");
+                }
+                break;
+
+            case "5":
+                Console.Write("Enter the word to remove:");
+                var wordToRemove = Console.ReadLine();
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    if (list[i].Contains(wordToRemove!))
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+                logger.WriteLog("INFO", $"Removed all lines containing the word: {wordToRemove}");
+                break;                
+
+            case "6":
+                list.Sort();
+                Console.WriteLine("Lines sorted.");
+                logger.WriteLog("INFO", "Sorted the lines in the list.");
+                break;
+                
+            case "0":
+                Console.WriteLine("Exiting...");
+                logger.WriteLog("INFO", "User chose to exit the application.");
+                break;
+
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                logger.WriteLog("WARNING", $"User entered an invalid option: {option}");
+                break;
+                
+        }
+    } while (option != "0");
+    texFile.WriteAllLines(list.ToArray());
+    Console.WriteLine("Changes saved.");
+    logger.WriteLog("INFO", "Application exited.");
+}
+catch (Exception ex)
+{
+    logger.WriteLog("ERROR", $"An error occurred: {ex.Message}");
+    Console.WriteLine("An error occurred. Please check the log for details.");
+}
+finally
+{
+    logger.WriteLog("INFO", "Application ended.");
+}
 
 
 string Menu()
@@ -86,7 +113,7 @@ string Menu()
     return Console.ReadLine() ?? string.Empty;
 }
 
-foreach (var line in lines)
-{
-    Console.WriteLine(line);
-}
+//foreach (var line in lines)
+//{
+//    Console.WriteLine(line);
+//}
